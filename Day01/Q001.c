408f2f36cdd4e3b10c0a75d39e9f2cdeef233531
dhruv <Dhruv.41014@stu.upes.ac.in>
2026-08-18 22:24:04 +0530

    start 100 Days Of Code

---
+++ Day01/Q001.c
@@ -0,0 +1,16 @@
+//write a program to imput two numbers and display their sum
+
+#include <stdio.h>
+
+int main() {
+
+ int a, b, sum;
+
+ printf("enter two number:");
+ scanf("%d %d", &a, &b);
+
+ sum = a + b;
+
+ printf("sum = %d/n", sum);
+ return 0;
+}
