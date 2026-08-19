142eae48ad9f75bbd821fff02a0844ab0d22f28a
dhruv <Dhruv.41014@stu.upes.ac.in>
2026-08-19 20:02:15 +0530

    Add Day02

---
+++ Day02/history/Q003_20260819_184428.c
@@ -0,0 +1,13 @@
+//write a program to calclate the area and perimeter of a rectangle given its length and breadth.
+
+#include <stdio.h>
+
+int main() {
+int length, breadth;
+scanf("%d %d", &leanth, &breadth);
+
+printf("area = %d/n", length * breadth);
+printf("perimeter = %d/n", 2 * (length + breadth);)
+return 0;
+
+}
---
+++ Day02/history/Q004_20260819_191618.c
@@ -0,0 +1,18 @@
+//write a program to calculate the area and circumference of a circle given its radius
+
+#include <stdio.h>
+
+int main() {
+float radius, area, circumference;
+float pi = 3.14;
+
+scanf("%f", &radius);
+
+area = pi * radius * radius;
+circumference = 2 * pi * r;
+
+printf("area = %.2f\n", area);
+printf("circumference = %.2f\n", circumference);
+return 0;
+
+}
