39d2b3f284491bb12a3a21145a6c40515b4aef52
dhruv <Dhruv.41014@stu.upes.ac.in>
2026-08-22 18:23:53 +0530

    Add Day05

---
+++ Day05/history/Q009_20260822_181110.c
@@ -0,0 +1,16 @@
+//write a program to calculate simple and compound interest for given principal, rate, time.
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+float p, r, t, si, ci;
+
+scanf("%f %f %f", &p, &r, &t);
+
+si = (p * r * t) / 100;
+ci = (1 + r / 100, t) -p;
+printf("simple intrest=%.0f, compound interest=%.1f", si, ci);
+return 0;
+
+}
