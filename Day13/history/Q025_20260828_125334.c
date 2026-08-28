09ab340947c60a0bb3a7bfacbe763c811fcc97be
dhruv <Dhruv.41014@stu.upes.ac.in>
2026-08-28 13:04:03 +0530

    Add Day13

---
+++ Day13/history/Q025_20260828_125334.c
@@ -0,0 +1,36 @@
+//write a program to implement a basic calcuulator using switch-case for +, -, /, %. 
+
+#include <stdio.h>
+
+int main() {
+int a, b;
+char op;
+scanf("%d %d %c", &a &b, &op);
+switch (op) {
+    case '+': 
+    printf("%d", a + b);
+    break;
+    case '-':
+    printf("%d", a - b);
+    break;
+    case '*':
+    printf("%d", a * b);
+    break;
+    case '/':
+    if(b != 0)
+    printf("%d", a / b);
+    else
+    printf("cannot divided by zero");
+    break;
+    case '%':
+    if(b != 0)
+    printf("%d", a % b);
+    else 
+    printf("cannot divided by 0");
+    break;
+    default:
+    printf("invalid operator")
+}
+    return 0;
+
+}
---
+++ Day13/history/Q026_20260828_130056.c
@@ -0,0 +1,13 @@
+//write a program to print numbers from 1 to n.
+
+#include <stdio.h>
+
+int main() {
+int n, 1;
+scanf("%d", &n);
+for(i = 1; i <= n; i++) {
+    printf("%d", i);
+}
+    return 0;
+
+}
