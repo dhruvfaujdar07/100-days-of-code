e27916404c58325ae2364fc10fb2c5c40759d024
dhruv <Dhruv.41014@stu.upes.ac.in>
2026-08-30 22:49:23 +0530

    Add Day16

---
+++ Day16/history/Q032_20260830_224750.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+int n, origional, reverse = 0, rem;
+scanf("%d", &n);
+original = n;
+while (n != 0)
+{
+    rem = n % 10;
+    reverse = reverse * 10 + rem;
+    n = n / 10;
+}
+if (origional == reverse)
+printf("palindrome");
+else
+printf("not Palindrome");
+    return 0;
+
+}
