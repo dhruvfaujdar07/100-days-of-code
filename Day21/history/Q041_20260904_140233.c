ab05ce80af4e77af7496a4f5310cc97d919849ce
dhruv <Dhruv.41014@stu.upes.ac.in>
2026-09-04 14:09:00 +0530

    Add Day21

---
+++ Day21/history/Q041_20260904_140233.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+int  num, first, last, temp, divisor = 1, middle,result;
+scanf("%d", &num);
+last = num % 10;
+temp = num;
+while (temp >= 10)
+{
+    temp = temp / 10;
+    divisor = divisor * 10;
+}
+first = temp;
+middle = (num % divisor) / 10;
+result = last * divisor + middle * 10 + first;
+printf("%d", reault);
+    return 0;
+
+}
