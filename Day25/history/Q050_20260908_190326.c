d74a99c6572d90b48eee8bf3b4a7535988e31221
dhruv <Dhruv.41014@stu.upes.ac.in>
2026-09-08 19:07:54 +0530

    Add Day25

---
+++ Day25/history/Q050_20260908_190326.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+int i, j;
+for(i = j; i <= 5; i++) {
+    for(j = 5; j >= 1; j--)
+    {
+        printf("*", j);
+    }
+    printf("\n");
+}
+    return 0;
+
+}
