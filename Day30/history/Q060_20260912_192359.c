b61750cd8c31c9f3397236ab633c9a5f38b7ecec
dhruv <Dhruv.41014@stu.upes.ac.in>
2026-09-12 19:32:26 +0530

    Add Day30

---
+++ Day30/history/Q060_20260912_192359.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+int n, i;
+int positive = 0, negative = 0, zero = 0;
+scanf("%d", &n);
+int arr[n];
+for(i = 0, i < n; i++)
+{
+    scanf("%d", &arr[i]);
+}
+for(i = 0; i < n; i++)
+{
+    if(arr[i] > 0)
+    {
+        positive++;
+    } else if(arr[i] < 0)
+    {
+        negative++;
+    } else {
+        zero;
+    }
+}
+    return 0;
+
+}
