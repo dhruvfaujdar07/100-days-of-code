888e244e0c13758ca72b0f88927a1e77ced45ebc
dhruv <Dhruv.41014@stu.upes.ac.in>
2026-09-13 11:26:54 +0530

    Add Day31

---
+++ Day31/history/Q061_20260913_112136.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+int n, i, key;
+scanf("%d", &n);
+int arr[n];
+for(i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+}
+scanf("%d", &key);
+for(i = 0; i < n; i++) {
+    if(arr[i] == key) {
+        printf("found at index %d", i);
+        return 0;
+    }
+}
+    printf("-1");
+    return 0;
+
+}
